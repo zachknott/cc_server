6507b4999e722e645123955b0958464d5facfcce
Zachary Knott <zacharyknott.19@gmail.com>
2022-08-03 14:23:28 +0000

    finished multi threading, will begin actual project later

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -6,17 +6,23 @@ use std::io::prelude::*;
 use std::thread;
 use std::time::Duration;
 
+mod ThreadTools;
+
+use ThreadTools::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(10);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
         pool.execute(|| {
             handle_connection(stream)
         });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
