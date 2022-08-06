5c3b2173f49d7b16f8fbc8f4a2f9db4eb3fb28a8
Zach Knott <zachary.knott@duke.edu>
2022-08-05 23:25:09 -0400

    New ip config

--- cc_client/src/main.rs
+++ cc_client/src/main.rs
@@ -7,7 +7,7 @@ use sys_config::ClientConfig;
 fn main() {
     let system = ClientConfig::new();
     
-    if let Ok(mut stream) = TcpStream::connect("127.0.0.1:7878") {
+    if let Ok(mut stream) = TcpStream::connect("192.168.1.89:7878") {
         
         println!("Connected to server...");
         stream.write(system.to_string().as_bytes()).unwrap();
